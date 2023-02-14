0bf38ab16900ca8852caedb7469cee5f1e6196b0
Prudhvi Raj <120363980+prudhvi1601@users.noreply.github.com>
2023-02-14 11:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
 
+var randomNum;
+var input;
+
+function generateRandomNum() {
+  randomNum = Math.floor(Math.random() * 41) - 20;
+  document.getElementById("num").innerText = randomNum;
+  guessNum();
+  checkGame();
+}
+function guessNum() {
+  input = Number(document.getElementById("guess").value);
+}
+function checkGame() {
+  if (Math.abs(randomNum - input <= 5)) {
+    document.getElementById("respond").innerText = "Hot";
+  }
+  else {
+    document.getElementById("respond").innerText = "Cold";
+  }
+}
+btn.addEventListener("click",generateRandomNum);
